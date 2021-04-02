eab4cc6580ebcba9c4eac134e8e49e4d62e384d7
nirbheeksetia <singhnirbheek@gmail.com>
2021-04-02 13:07:15 +0200

    added method for multiplication

--- calculator.py
+++ calculator.py
@@ -1,6 +1,28 @@
-def add():
-    one = input("Number One:")
-    two = input("Number Two:")
-    print( int(one) + int(two))
+if __name__ == "__main__":
+    def add():
+        one = input("Number One:")
+        two = input("Number Two:")
+        print( int(one) + int(two))
 
-add()
+    def multiply():
+        three = input("Number three:")
+        four = input("Number four:")
+        m = int(three) * int(four)
+        print(m)
+
+    addormultiply = input("Wuld you like to add or multiply?(add/multiply):")
+
+    if addormultiply != "add" or addormultiply != "multiply":  
+        print("Wrong Input!")
+        #addormultiply = input("Wuld you like to add or multiply?(add/multiply):")
+        multiply()
+
+    elif addormultiply == "multiply":
+        multiply()
+    elif addormultiply == "add":
+        add()
+
+
+
+
+
